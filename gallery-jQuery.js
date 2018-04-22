16aa0a0e41eec1702b2a129c96c0afcf331d5424
JamesMcArrow <16477316+JamesMcarrow@users.noreply.github.com>
2018-04-22 21:14:09 +0200

    info,vzorek, zmeny v jQuery, kontakty

--- gallery-jQuery.js
+++ gallery-jQuery.js
@@ -5,83 +5,102 @@ $(document).ready(function() {
  //galerie
         $("#1").click(function() {
               $('#gallery-full').css("background-image","url('gal_img_1.jpg')");
-              
+
         });
-        
+
         $("#2").click(function() {
               $('#gallery-full').css("background-image","url('gal_img_2.jpg')");
-              
+
         });
-        
+
         $("#3").click(function() {
               $('#gallery-full').css("background-image","url('gal_img_3.jpg')");
-              
+
         });
-        
+
         $("#4").click(function() {
               $('#gallery-full').css("background-image","url('gal_img_4.jpg')");
-              
+
         });
-        
+
         $("#5").click(function() {
               $('#gallery-full').css("background-image","url('gal_img_5.jpg')");
-              
+
         });
 // konec galerie
-// rozkliky 
+// rozkliky
 
         $('.line-artist-detail').hide();
-          
+
         $('#first-artist').click(function(){
-            
-            $('#first-artist-detail').toggle(300);   
-              
+
+            $('#first-artist-detail').toggle(300);
+
               $('#second-artist-detail').hide(150);
               $('#third-artist-detail').hide(150);
               $('#fourth-artist-detail').hide(150);
               $('#fifth-artist-detail').hide(150);
+              $('#sixth-artist-detail').hide(150);
           });
-          
+
         $('#second-artist').click(function(){
-            
-            $('#second-artist-detail').toggle(300);   
-              
+
+            $('#second-artist-detail').toggle(300);
+
               $('#first-artist-detail').hide(150);
               $('#third-artist-detail').hide(150);
               $('#fourth-artist-detail').hide(150);
               $('#fifth-artist-detail').hide(150);
+              $('#sixth-artist-detail').hide(150);
           });
-          
+
         $('#third-artist').click(function(){
-            
-            $('#third-artist-detail').toggle(300);   
-              
+
+            $('#third-artist-detail').toggle(300);
+
               $('#second-artist-detail').hide(150);
               $('#first-artist-detail').hide(150);
               $('#fourth-artist-detail').hide(150);
               $('#fifth-artist-detail').hide(150);
+              $('#sixth-artist-detail').hide(150);
           });
-          
+
         $('#fourth-artist').click(function(){
-            
-            $('#fourth-artist-detail').toggle(300);   
-              
+
+            $('#fourth-artist-detail').toggle(300);
+
               $('#second-artist-detail').hide(150);
               $('#third-artist-detail').hide(150);
               $('#first-artist-detail').hide(150);
               $('#fifth-artist-detail').hide(150);
+              $('#sixth-artist-detail').hide(150);
           });
-          
+
         $('#fifth-artist').click(function(){
-            
-            $('#fifth-artist-detail').toggle(300);   
-              
+
+            $('#fifth-artist-detail').toggle(300);
+
               $('#second-artist-detail').hide(150);
               $('#third-artist-detail').hide(150);
               $('#fourth-artist-detail').hide(150);
               $('#first-artist-detail').hide(150);
+              $('#sixth-artist-detail').hide(150);
+         });
+
+        $('#sixth-artist').click(function(){
+
+            $('#sixth-artist-detail').toggle(300);
+
+              $('#second-artist-detail').hide(150);
+              $('#third-artist-detail').hide(150);
+              $('#fourth-artist-detail').hide(150);
+              $('#first-artist-detail').hide(150);
+              $('#fifth-artist-detail').hide(150);
           });
- 
+// divadlo prouzky
+        
+
+//smooth scroll
           $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
@@ -89,8 +108,8 @@ $(document).ready(function() {
   .click(function(event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
